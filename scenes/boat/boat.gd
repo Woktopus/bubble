625d9d0353bd9c189743b4e9dce7891c2ba20452
Hadjin <augustemeyer86@gmail.com>
2025-01-25 00:04:27 +0100

    init boat moove and fiction

--- scenes/boat/boat.gd
+++ scenes/boat/boat.gd
@@ -1,11 +1,31 @@
-extends Node2D
+extends CharacterBody2D
+class_name Player
+
+const BASE_SPEED = 500.0
+const BASE_ACCELERATION = 200.0
+
+var friction : float = BASE_ACCELERATION / BASE_SPEED
+var input : Vector2
+
+func get_input():
+	input.x = Input.get_action_strength("player1_right") - Input.get_action_strength("player1_left")
+	input.y = Input.get_action_strength("player1_down") - Input.get_action_strength("player1_up")
+	return input.normalized()
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	apply_traction(delta)
+	apply_friction(delta)
+	move_and_slide()
+
+	
+func apply_traction(delta: float)-> void:
+	var traction = get_input()
+	velocity += traction * BASE_ACCELERATION  * delta
+	
+func apply_friction(delta: float)-> void:
+	velocity -= velocity * friction * delta
+	
+	
+	
