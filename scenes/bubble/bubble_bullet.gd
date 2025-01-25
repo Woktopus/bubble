12c33ac37d65ecc9b4747dd2883cd880c85213aa
Hadjin <augustemeyer86@gmail.com>
2025-01-25 18:48:46 +0100

    edit bubble interaction

--- scenes/bubble/bubble_bullet.gd
+++ scenes/bubble/bubble_bullet.gd
@@ -8,15 +8,21 @@ var direcion : Vector2
 @onready var visible_notifier = $VisibleNotifier as VisibleOnScreenNotifier2D
 
 var direction = Vector2.RIGHT
+var state_alive :bool
+var player_owner : int = -1
 
 func _ready():
 	pass
 
+func setup_bubble(player_id:int)->void :
+	state_alive = true
+	player_owner = player_id
+	
+
 func _physics_process(delta: float) -> void:
 	move_bubble(delta)
 	
-func _setup_bubble() -> void:
-	pass
+
 
 func move_bubble(delta : float) -> void:
 	#move_and_collide(direcion * bullet_speed * delta)
@@ -30,3 +36,16 @@ func move_bubble(delta : float) -> void:
 
 func bubble_explode() -> void:
 	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if "Boat" in body.name :
+		
+		print("========================")
+		prints("owner id %s ", player_owner)
+		prints("target id target %s", body.get_play_id())
+		print("========================")
+		if body.get_play_id() != player_owner:
+			queue_free()
+			body.manage_bubble_hit()
+		#print(state_alive)
