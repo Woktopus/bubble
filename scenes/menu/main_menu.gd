e7062375cd4d10fbaa5a100af032b7959a172620
Gryfenwill <romjan@orange.fr>
2025-01-26 14:51:22 +0100

    add link menu to credits

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -13,7 +13,7 @@ func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/lobby/lobby.tscn")
 
 func _on_credit_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/credit/credit.tscn")
 
 func _on_exitbutton_pressed() -> void:
 	get_tree().quit()
