6dd798803472f2c6a7ce7f98246447dafefaca91
Gryfenwill <romjan@orange.fr>
2025-01-26 01:45:04 +0100

    add coin

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -21,3 +21,16 @@ func _on_credit_button_pressed() -> void:
 
 func _on_exitbutton_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_start_button_mouse_entered() -> void:
+	pass # Replace with function body.
+	$VBoxContainer/exitbutton/AudioStreamPlayer.play()
+
+func _on_credit_button_mouse_entered() -> void:
+	pass # Replace with function body.
+	$VBoxContainer/exitbutton/AudioStreamPlayer.play()
+
+func _on_exitbutton_mouse_entered() -> void:
+	pass # Replace with function body.
+	$VBoxContainer/exitbutton/AudioStreamPlayer.play()
