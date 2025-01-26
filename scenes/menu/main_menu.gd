6e2f54ac0516e5aa2fbc4a62c0100a859d14a6a9
Gryfenwill <romjan@orange.fr>
2025-01-26 11:29:32 +0100

    add wiggling title

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -5,23 +5,18 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/lobby/lobby.tscn")
-
 
 func _on_credit_button_pressed() -> void:
 	pass # Replace with function body.
 
-
 func _on_exitbutton_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_start_button_mouse_entered() -> void:
 	pass # Replace with function body.
