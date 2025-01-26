418b39fe80d0eae54718f73704500d96470720bb
Briland Mathieu <briland.mathieu@gmail.com>
2025-01-26 16:38:03 +0100

    add select with controller on main menu

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -3,11 +3,17 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$VBoxContainer/startButton.grab_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_accept"):
+		if $VBoxContainer/startButton.has_focus():
+			_on_start_button_pressed()
+		elif $VBoxContainer/creditButton.has_focus():
+			_on_credit_button_pressed()
+		elif $VBoxContainer/exitButton.has_focus():
+			_on_exitbutton_pressed()
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/lobby/lobby.tscn")
